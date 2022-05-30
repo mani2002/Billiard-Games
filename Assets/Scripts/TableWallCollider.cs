bcc8f92323fe540994d5dab57eb7d9f17ef4a015
mani2002 <61064881+mani2002@users.noreply.github.com>
2022-05-30 20:22:59 +0530

    Add files via upload

---
+++ Assets/Scripts/TableWallCollider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using BilliardGame.Controllers;
+using BilliardGame.Managers;
+
+namespace BilliardGame
+{
+    public class TableWallCollider : MonoBehaviour
+    {
+        private void OnTriggerStay(Collider collider)
+        {
+            CueBallController cueBallController = collider.gameObject.GetComponent<CueBallController>();
+            if (cueBallController != null && cueBallController.GetComponent<Rigidbody>().IsSleeping())
+            {
+                GameManager.Instance.AddToBallHitOutList(cueBallController);
+                //AudioSource audioSource = GetComponent<AudioSource>();
+                //audioSource.Play();
+            }
+             
+        }
+       
+       
+    }
+}
